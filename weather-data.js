1a7214b906681466ca52c3a9398b70b56c7a9b4c
Blair Betts <blairbetts25@gmail.com>
2019-03-27 21:19:47 -0400

    added vars

--- weather-data.js
+++ weather-data.js
@@ -2,10 +2,31 @@ $(document).ready(function () {
     var apiKey = "bf0fe2b0776e786b0b03723a094e6d31";
     var citySelected;
     var daysForcasted = 5
-    var humidity;
-    var temp;
-    var description;
-    var windSpeed;
+    var dayOneHumidity;
+    var dayOneTemp;
+    var dayOneDescription;
+    var dayOneWind;
+    var dayOneDate;
+    var dayTwoHumidity;
+    var dayTwoTemp;
+    var dayTwoDescription;
+    var dayTwoWind;
+    var dayTwoDate;
+    var dayThreeHumidity;
+    var dayThreeTemp;
+    var dayThreeDescription;
+    var dayThreeWind;
+    var dayThreeDate;
+    var dayFourHumidity;
+    var dayFourTemp;
+    var dayFourDescription;
+    var dayFourWind;
+    var dayFourDate;
+    var dayFiveHumidity;
+    var dayFiveTemp;
+    var dayFiveDescription;
+    var dayFiveWind;
+    var dayFiveDate;
     var config = {
         apiKey: "AIzaSyD2ZqCn7UFWz3oqrz2slSa5rU1IATRuKg0",
         authDomain: "brass-monkey-project.firebaseapp.com",
@@ -13,12 +34,12 @@ $(document).ready(function () {
         projectId: "brass-monkey-project",
         storageBucket: "brass-monkey-project.appspot.com",
         messagingSenderId: "384344100286"
-      };
-      firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
     var database = firebase.database();
 
-    function createButtons(){
-        for(i=0; i<daysForcasted; i++){
+    function createButtons() {
+        for (i = 0; i < daysForcasted; i++) {
             var newButton = $("<button>");
             newButton.attr("data", [i]);
             $("#date").append(newButton);
@@ -41,13 +62,13 @@ $(document).ready(function () {
                 console.log(response.list[0].weather[0].description);
                 console.log("wind speed " + response.list[0].wind.speed);
                 console.log(response.list[0].dt_txt);
-                
-                var dayOneHumidity = "humidity " + response.list[0].main.humidity + "%";
-                var dayOneTemp = "temp " + response.list[0].main.temp + " F";
-                var dayOneDescription = response.list[0].weather[0].description;
-                var dayOneWind = "wind speed " + response.list[0].wind.speed + " MPH";
-                var dayOneDate = response.list[0].dt_txt;
-                
+
+                dayOneHumidity = "humidity " + response.list[0].main.humidity + "%";
+                dayOneTemp = "temp " + response.list[0].main.temp + " F";
+                dayOneDescription = response.list[0].weather[0].description;
+                dayOneWind = "wind speed " + response.list[0].wind.speed + " MPH";
+                dayOneDate = response.list[0].dt_txt;
+
                 var dayOne = {
                     dayOneTemp: dayOneTemp,
                     dayOneHumidity: dayOneHumidity,
@@ -55,11 +76,11 @@ $(document).ready(function () {
                     dayOneWind: dayOneWind,
                     dayOneDate: dayOneDate
                 };
-                var dayTwoHumidity = "humidity " + response.list[8].main.humidity + "%";
-                var dayTwoTemp = "temp " + response.list[8].main.temp + " F";
-                var dayTwoDescription = response.list[8].weather[0].description;
-                var dayTwoWind = "wind speed " + response.list[8].wind.speed + " MPH";
-                var dayTwoDate = response.list[8].dt_txt;
+                dayTwoHumidity = "humidity " + response.list[8].main.humidity + "%";
+                dayTwoTemp = "temp " + response.list[8].main.temp + " F";
+                dayTwoDescription = response.list[8].weather[0].description;
+                dayTwoWind = "wind speed " + response.list[8].wind.speed + " MPH";
+                dayTwoDate = response.list[8].dt_txt;
                 var dayTwo = {
                     dayTwoTemp: dayTwoTemp,
                     dayTwoHumidity: dayTwoHumidity,
@@ -67,11 +88,11 @@ $(document).ready(function () {
                     dayTwoWind: dayTwoWind,
                     dayTwoDate: dayTwoDate
                 };
-                var dayThreeHumidity = "humidity " + response.list[16].main.humidity + "%";
-                var dayThreeTemp = "temp " + response.list[16].main.temp + " F";
-                var dayThreeDescription = response.list[16].weather[0].description;
-                var dayThreeWind = "wind speed " + response.list[16].wind.speed + " MPH";
-                var dayThreeDate = response.list[16].dt_txt;
+                dayThreeHumidity = "humidity " + response.list[16].main.humidity + "%";
+                dayThreeTemp = "temp " + response.list[16].main.temp + " F";
+                dayThreeDescription = response.list[16].weather[0].description;
+                dayThreeWind = "wind speed " + response.list[16].wind.speed + " MPH";
+                dayThreeDate = response.list[16].dt_txt;
                 var dayThree = {
                     dayThreeTemp: dayThreeTemp,
                     dayThreeHumidity: dayThreeHumidity,
@@ -79,11 +100,11 @@ $(document).ready(function () {
                     dayThreeWind: dayThreeWind,
                     dayThreeDate: dayThreeDate
                 };
-                var dayFourHumidity = "humidity " + response.list[24].main.humidity + "%";
-                var dayFourTemp = "temp " + response.list[24].main.temp + " F";
-                var dayFourDescription = response.list[24].weather[0].description;
-                var dayFourWind = "wind speed " + response.list[24].wind.speed + " MPH";
-                var dayFourDate = response.list[24].dt_txt;
+                dayFourHumidity = "humidity " + response.list[24].main.humidity + "%";
+                dayFourTemp = "temp " + response.list[24].main.temp + " F";
+                dayFourDescription = response.list[24].weather[0].description;
+                dayFourWind = "wind speed " + response.list[24].wind.speed + " MPH";
+                dayFourDate = response.list[24].dt_txt;
                 var dayFour = {
                     dayFourTemp: dayFourTemp,
                     dayFourHumidity: dayFourHumidity,
@@ -91,12 +112,12 @@ $(document).ready(function () {
                     dayFourWind: dayFourWind,
                     dayFourDate: dayFourDate
                 };
-                var dayFiveHumidity = "humidity " + response.list[32].main.humidity + "%";
-                var dayFiveTemp = "temp " + response.list[32].main.temp + " F";
-                var dayFiveDescription = response.list[32].weather[0].description;
-                var dayFiveWind = "wind speed " + response.list[32].wind.speed + " MPH";
-                var dayFiveDate = response.list[32].dt_txt;
-                var dayFive = {
+                dayFiveHumidity = "humidity " + response.list[32].main.humidity + "%";
+                dayFiveTemp = "temp " + response.list[32].main.temp + " F";
+                dayFiveDescription = response.list[32].weather[0].description;
+                dayFiveWind = "wind speed " + response.list[32].wind.speed + " MPH";
+                dayFiveDate = response.list[32].dt_txt;
+                dayFive = {
                     dayFiveTemp: dayFiveTemp,
                     dayFiveHumidity: dayFiveHumidity,
                     dayFiveDescription: dayFiveDescription,
@@ -111,12 +132,15 @@ $(document).ready(function () {
                 database.ref().push(dayFive);
 
 
-                
+
                 var mapURL = "https://openweathermap.org/weathermap?basemap=map&cities=false&layer=pressure&lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&zoom=10";
                 console.log(mapURL);
                 $("#map").append(mapURL);
                 $("#map").attr("href", "https://openweathermap.org/weathermap?basemap=map&cities=false&layer=pressure&lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon + "&zoom=10");
                 createButtons();
             })
+    })
+    database.ref().on("child_added", function (childSnapshot) {
+
     })
 })
